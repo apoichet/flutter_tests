e5f895dd9b04adf87eca43c3c109b0df85782f08
Poichet Alexandre <alexandre_poichet@lm-fvfg92cbq05p.groupevsc.com>
2021-10-18 13:41:35 +0200

    update script execute tests

--- execute_tests.bash
+++ execute_tests.bash
@@ -2,23 +2,29 @@
 
 # Execute tests
 echo "Execute tests"
-echo "fvm flutter --suppress-analytics test --coverage lib > test_report/tests.output"
-fvm flutter --suppress-analytics test --machine --coverage --coverage-path=coverage/lcov.tests.info > test_report/tests.output || echo 'App tests failed'
-echo "fvm flutter --suppress-analytics pub global run junitreport:tojunit --input test_report/tests.output --output test_report/tests-junit.xml"
-fvm flutter --suppress-analytics pub global run junitreport:tojunit --input test_report/tests.output --output test_report/tests-junit.xml
+flutter_test="fvm flutter --suppress-analytics test --machine --coverage --coverage-path=coverage/lcov.tests.info --test-randomize-ordering-seed=random > test_report/tests.output"
+echo "$flutter_test"
+eval "$flutter_test" || echo 'App tests failed'
+flutter_test_report="fvm flutter --suppress-analytics pub global run junitreport:tojunit --input test_report/tests.output --output test_report/tests-junit.xml"
+echo "$flutter_test_report"
+eval "$flutter_test_report"
 
 # Execute integration tests
 echo "Execute integration tests"
-echo "fvm flutter --suppress-analytics test integration_tests --coverage lib --merge-coverage > test_report/tests-integration.output"
-fvm flutter --suppress-analytics test integration_test --machine --coverage --coverage-path=coverage/lcov.tests-integration.info --dart-define=INTEGRATION_TEST=true > test_report/tests-integration.output || echo 'App tests failed'
-echo "fvm flutter --suppress-analytics pub global run junitreport:tojunit --input test_report/tests-integration.output --output test_report/tests-integration-junit.xml"
-fvm flutter --suppress-analytics pub global run junitreport:tojunit --input test_report/tests-integration.output --output test_report/tests-integration-junit.xml
+flutter_integration_test="fvm flutter --suppress-analytics test integration_tests --machine --coverage --coverage-path=coverage/lcov.tests-integration.info --dart-define=INTEGRATION_TEST=true > test_report/tests-integration.output"
+echo "$flutter_integration_test"
+eval "$flutter_integration_test"
+flutter_integration_test_report="fvm flutter --suppress-analytics pub global run junitreport:tojunit --input test_report/tests-integration.output --output test_report/tests-integration-junit.xml"
+echo "$flutter_integration_test_report"
+eval "$flutter_integration_test_report"
 
 # Generate coverage report
 echo "Generate coverage report"
-echo "lcov --add-tracefile coverage/lcov.tests.info -a coverage/lcov.tests-integration.info -o coverage/lcov.merged.info"
-lcov --add-tracefile coverage/lcov.tests.info -a coverage/lcov.tests-integration.info -o coverage/lcov.merged.info
+lcov_merge_coverage="lcov --add-tracefile coverage/lcov.tests.info -a coverage/lcov.tests-integration.info -o coverage/lcov.merged.info"
+echo "$lcov_merge_coverage"
+eval "$lcov_merge_coverage"
 # Remove repository fake coverage
 lcov --remove coverage/lcov.merged.info 'lib/repository/traveler_repository_fake.dart'
-echo "genhtml coverage/lcov.merged.info -o coverage/html"
-genhtml coverage/lcov.merged.info -o coverage/html
+gen_report_html="genhtml coverage/lcov.merged.info -o coverage/html"
+echo "$gen_report_html"
+eval "$gen_report_html"
